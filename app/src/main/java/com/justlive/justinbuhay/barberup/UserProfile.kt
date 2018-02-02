9fbbc96480e7d5f48617ab66bd95076de899fefb
JBgithub34 <jbuhay3@gmail.com>
2018-02-01 16:54:01 -0800

    Added TabLayout onto the app, no more viewpager strip

--- app/src/main/java/com/justlive/justinbuhay/barberup/UserProfile.kt
+++ app/src/main/java/com/justlive/justinbuhay/barberup/UserProfile.kt
@@ -3,6 +3,7 @@ package com.justlive.justinbuhay.barberup
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
 import android.support.v4.app.FragmentManager
@@ -13,14 +14,22 @@ class UserProfile : FragmentActivity() {
 
     private var mProfileFragmentPagerAdapter: ProfileFragmentPagerAdapter? = null
     private var mViewPager: ViewPager? = null
+    private var mTabLayout: TabLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
 
+        mTabLayout = findViewById(R.id.tablayout)
+        mTabLayout!!.addTab(mTabLayout!!.newTab().setText("BarberInformation"))
+        mTabLayout!!.addTab(mTabLayout!!.newTab().setText("Availability"))
+
         mProfileFragmentPagerAdapter = ProfileFragmentPagerAdapter(supportFragmentManager)
         mViewPager = findViewById(R.id.pager)
+
+
         mViewPager!!.setAdapter(mProfileFragmentPagerAdapter)
+        mTabLayout!!.setupWithViewPager(mViewPager)
     }
 
     class ProfileFragmentPagerAdapter: FragmentPagerAdapter{
