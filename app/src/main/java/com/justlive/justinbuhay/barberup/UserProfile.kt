d840bfe1073b75c68fa09d45cf8c32f9d628e0e3
JBgithub34 <jbuhay3@gmail.com>
2018-02-01 16:34:31 -0800

    Created TabLayout using ViewPager on the profile page

--- app/src/main/java/com/justlive/justinbuhay/barberup/UserProfile.kt
+++ app/src/main/java/com/justlive/justinbuhay/barberup/UserProfile.kt
@@ -2,11 +2,56 @@ package com.justlive.justinbuhay.barberup
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 
-class UserProfile : AppCompatActivity() {
+class UserProfile : FragmentActivity() {
+
+    private var mProfileFragmentPagerAdapter: ProfileFragmentPagerAdapter? = null
+    private var mViewPager: ViewPager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+
+        mProfileFragmentPagerAdapter = ProfileFragmentPagerAdapter(supportFragmentManager)
+        mViewPager = findViewById(R.id.pager)
+        mViewPager!!.setAdapter(mProfileFragmentPagerAdapter)
+    }
+
+    class ProfileFragmentPagerAdapter: FragmentPagerAdapter{
+
+        constructor(fm: FragmentManager) : super(fm)
+
+
+        override fun getCount(): Int {
+            return 2
+        }
+
+        override fun getItem(position: Int): Fragment {
+            when(position){
+                0-> return BarberProfileFragment()
+                1-> return BarberAvailabilityFragment()
+
+                else ->
+                        return BarberProfileFragment()
+            }
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            when(position){
+                0-> return "Barber Information"
+                1-> return "Availability"
+
+                else ->
+                    return "Barber Information"
+            }
+        }
+
+
     }
 }
